cbf8e2f785dae55f0a6613a334e830c177438700
Philip Schlump <pschlump@gmail.com>
2015-09-08 07:07:16 -0600

    Initial Checkin

---
+++ jsonp.go
@@ -0,0 +1,59 @@
+package jsonp
+
+// Copyright (C) Philip Schlump, 2013-2015.
+// Version: 1.0.0
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+)
+
+//
+// Example of Use
+//
+// In a handler you build some JSON then call JsonP on the return value.
+//
+//	 	func handleVersion(res http.ResponseWriter, req *http.Request) {
+// 			res.Header().Set("Content-Type", "application/json")
+// 			io.WriteString(res, jsonp.JsonP(fmt.Sprintf(`{"status":"success", "version":"1.0.0"}`+"\n"), res, req))
+// 		}
+//
+
+var JSON_Prefix string = ""
+
+// Set a prefix that will be pre-pended to every return JSON string.
+func SetJsonPrefix(p string) {
+	JSON_Prefix = p
+}
+
+// Take a string 's' in JSON and if a get parameter "callback" is specified then format this for JSONP callback.
+// If it is not a JSONp call (no "callback" parameter) then add JSON_Prefix to the beginning.
+func JsonP(s string, res http.ResponseWriter, req *http.Request) string {
+
+	u, _ := url.ParseRequestURI(req.RequestURI)
+	m, _ := url.ParseQuery(u.RawQuery)
+	callback := m.Get("callback")
+	if callback != "" {
+		res.Header().Set("Content-Type", "application/javascript")
+		return fmt.Sprintf("%s(%s);", callback, s)
+	} else {
+		return JSON_Prefix + s
+	}
+}
+
+// If "callback" is not "" then convert the JSON string 's' to a JSONp callback.
+// If it is not a JSONp call (no "callback" parameter) then add JSON_Prefix to the beginning.
+func JsonP_Param(s string, res http.ResponseWriter, callback string) string {
+	if callback != "" {
+		res.Header().Set("Content-Type", "application/javascript")
+		return fmt.Sprintf("%s(%s);", callback, s)
+	} else {
+		return JSON_Prefix + s
+	}
+}
+
+// For non-JSONP callable - just prepend the prefix and return.
+func PrependPrefix(s string) string {
+	return JSON_Prefix + s
+}
---
+++ jsonp_test.go
@@ -0,0 +1,74 @@
+package jsonp
+
+import (
+	"net/http"
+	"testing"
+)
+
+type TestAAAResponseWriter struct {
+	hdr    http.Header
+	buf    []byte
+	status int
+}
+
+func (ho TestAAAResponseWriter) Header() http.Header {
+	return http.Header(ho.hdr)
+}
+
+func (ho TestAAAResponseWriter) Write(b []byte) (int, error) {
+	return 0, nil
+}
+
+func (ho TestAAAResponseWriter) WriteHeader(code int) {
+	ho.status = code
+}
+
+func Test301Redirect(t *testing.T) {
+
+}
+
+func Test_Exists(t *testing.T) {
+	/*
+		if Exists("./no-such-file.do-not-create") {
+			t.Errorf("Expected false, file should not exist\n")
+		}
+		if !Exists("./test_data") {
+			t.Errorf("Expected true, check of directory\n")
+		}
+		if !Exists("./test_data/file-exists") {
+			t.Errorf("Expected true, check of directory\n")
+		}
+	*/
+	SetJsonPrefix(")]}',\n")
+	if JSON_Prefix != ")]}',\n" {
+		t.Errorf("Expected prefix to be set, not set correctly\n")
+	}
+
+	var bob = "{\"bob\":\"bob\"}\n"
+
+	if PrependPrefix(bob) != ")]}',\n{\"bob\":\"bob\"}\n" {
+		t.Errorf("Expected prefix to be used, not used correctly\n")
+	}
+
+	SetJsonPrefix("")
+
+	res := TestAAAResponseWriter{
+		hdr:    make(http.Header),
+		buf:    make([]byte, 0, 200),
+		status: 0,
+	}
+
+	s := JsonP_Param(bob, res, "callback000")
+	// fmt.Printf("s >%s<\n", s)
+	if s != "callback000({\"bob\":\"bob\"}\n);" {
+		t.Errorf("Expected JSONp callback, got >%s< \n", s)
+	}
+	// TODO: check that headers are set corectly
+
+	// req, _ := http.NewRequest("GET", "http://localhost:8204/api/status?id=xyzzy", nil)		// Test requries a server at this locaiton
+	req, _ := http.NewRequest("GET", "http://google.com/", nil)
+	_ = req
+
+	// TODO: func JsonP(s string, res http.ResponseWriter, req *http.Request) string {
+
+}
